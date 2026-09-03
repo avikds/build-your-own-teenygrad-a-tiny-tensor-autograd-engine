044b72bee05ce257dc0249641aac0fa1579540de
Avik Das <avikd957@gmail.com>
2026-09-03 13:42:23 +0530

    Step 36: tensor_creation_helpers

--- model.py
+++ model.py
@@ -567,8 +567,19 @@ def tensor_from_data(data, requires_grad=False):
         requires_grad=requires_grad,
     )
 
-# Step 36 - tensor_creation_helpers (not yet solved)
-# TODO: implement
+# Step 36 - tensor_creation_helpers
+def tensor_creation_helpers():
+    # Return helpers that create constant-filled Tensors.
+    def zeros_fn(shape):
+        return Tensor(LazyBuffer.const(0.0, shape))
+
+    def ones_fn(shape):
+        return Tensor(LazyBuffer.const(1.0, shape))
+
+    def full_fn(shape, value):
+        return Tensor(LazyBuffer.const(value, shape))
+
+    return zeros_fn, ones_fn, full_fn
 
 # Step 37 - tensor_randn (not yet solved)
 # TODO: implement
